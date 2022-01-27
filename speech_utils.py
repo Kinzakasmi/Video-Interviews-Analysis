0eaf6770382af7fac844084fb236edb9ce693a4e
Guiglaz <guilhem.glaziou@gmail.com>
2022-01-27 21:25:37 +0100

    Update SpeechRecognition function

--- speech_utils.py
+++ speech_utils.py
@@ -1,10 +1,12 @@
 import speech_recognition as sr 
 
-def speech_recognition(filename):
+
+def speech_recognition(audio):
+    """
+    Listen from preprocessed audio (ie silences has to be removed for Recognize to work) to
+    compute speech as a string
+    """
     r = sr.Recognizer()
-    audio = sr.AudioFile(filename)
     with audio as source:
         audio_file = r.record(source)
-    
-    result = r.recognize_google(audio_file,language = 'fr-FR', show_all=False)
-    return result
+    return r.recognize_google(audio_file,language = 'fr-FR', show_all=False)
