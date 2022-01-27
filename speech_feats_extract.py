20f93cf60e78a2e7a16eb9979fbd9d811fce840d
Guiglaz <guilhem.glaziou@gmail.com>
2022-01-27 19:57:18 +0100

    Adding lexical_features class

--- speech_feats_extract.py
+++ speech_feats_extract.py
@@ -3,13 +3,17 @@ from nltk import wordpunct_tokenize
 from nltk.corpus import stopwords
 from nltk.corpus import words
 from string import punctuation
+from speech_utils import *
+import audio_feats_extract
 
 class FrenchStemTokenizer(object):
+
     def __init__(self, remove_non_words=True):
         self.st = FrenchStemmer()
         self.stopwords = set(stopwords.words('french'))
         self.words = set(words.words())
         self.remove_non_words = remove_non_words
+
     def __call__(self, doc):
         # tokenize words and punctuation
         word_list = wordpunct_tokenize(doc)
@@ -23,3 +27,38 @@ class FrenchStemTokenizer(object):
         # remove non alpha
         word_list = [word for word in word_list if word.isalpha()]
         return [self.st.stem(t) for t in word_list]
+
+
+class LexicalFeatures:
+
+    def __init__(self, filename):
+
+        self.__speech = speech_recognition(filename)
+        self.word_count = 0
+        self.words_per_sentence = 0
+        self.letters_per_word = 0
+        self.dic_words = 0
+        self.func_words = 0
+        self.i = {}
+        self.article = {}
+        self.past = {}
+        self.present = {}
+        self.adverb = {}
+        self.social = {}
+        self.negemo = {}
+        self.sad = {}
+        self.work = {}
+        self.period = {}
+        self.average_word_len = 0
+        self.longest_sentence = 0
+        self.average_sentence_len = 0
+        self.type_token_ratio = 0
+        self.tot_vocab = 0
+        self.unique_word_count = 0
+        self.diff_word_count = 0
+        self.complexity = 0
+        self.rate_of_speech = 0
+
+    def __call__(self):
+        return vars(self)
+
